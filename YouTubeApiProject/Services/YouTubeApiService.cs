e9dc5bc180fdca62fc18e7c71f6ada07eea8b9fd
User <User@DESKTOP-A56R48H>
2024-10-18 20:22:48 +0800

    Add project files.

---
+++ YouTubeApiProject/Services/YouTubeApiService.cs
@@ -0,0 +1,50 @@
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using YouTubeApiProject.Models;
+
+namespace YouTubeApiProject.Services
+{
+    public class YouTubeApiService
+    {
+        private readonly YouTubeService _youtubeService;
+
+        public YouTubeApiService()
+        {
+            _youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyCCoF7NUedgNxL-9l9rsrXT16j6YVTHWMY",
+                ApplicationName = this.GetType().ToString()
+            });
+        }
+
+        public async Task<List<YouTubeVideoModel>> SearchVideosAsync(string query)
+        {
+            var searchListRequest = _youtubeService.Search.List("snippet");
+            searchListRequest.Q = query; // The search query
+            searchListRequest.MaxResults = 10;
+            searchListRequest.Type = "video"; // Ensure only videos are returned
+
+            var searchListResponse = await searchListRequest.ExecuteAsync();
+
+            var videos = new List<YouTubeVideoModel>();
+
+            foreach (var searchResult in searchListResponse.Items)
+            {
+                if (searchResult.Id.Kind == "youtube#video")
+                {
+                    videos.Add(new YouTubeVideoModel
+                    {
+                        Title = searchResult.Snippet.Title,
+                        Description = searchResult.Snippet.Description,
+                        ThumbnailUrl = searchResult.Snippet.Thumbnails.High.Url,
+                        VideoUrl = $"https://www.youtube.com/watch?v={searchResult.Id.VideoId}"
+                    });
+                }
+            }
+
+            return videos;
+        }
+    }
+}
